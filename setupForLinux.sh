9c7a6329e189d311fcac76483c8f21e3eb48fe3a
MLRIT-18R21A0551 <18r21a0551@mlrinstitutions.ac.in>
2022-05-25 20:17:57 +0530

    R package Repo

--- setupForLinux.sh
+++ setupForLinux.sh
@@ -1,4 +1,4 @@
-
+<<com
 sudo apt-get update
 sudo apt update
 yes Y | sudo apt install python3-pip
@@ -8,10 +8,13 @@ cd Dataset
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1CU-eImDIGflCKbPHGdkodgtMp6RDfUjY' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1CU-eImDIGflCKbPHGdkodgtMp6RDfUjY" -O "Dataset_LUNA_16.zip"&& rm -rf /tmp/cookies.txt
 yes Y | sudo apt-get install unzip
 unzip Dataset_LUNA_16
-
+com
 sudo apt update -qq
 sudo apt install --no-install-recommends software-properties-common dirmngr
 sudo wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 yes '\n' | sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 
 yes Y | sudo apt install --no-install-recommends r-base
+
+#add CRAN repositories
+yes '\n' | sudo add-apt-repository ppa:c2d4u.team/c2d4u4.0+
