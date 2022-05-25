0c2c75074bfd5a65524b2db42c4132ef50a5a364
MLRIT-18R21A0551 <18r21a0551@mlrinstitutions.ac.in>
2022-05-25 19:29:45 +0530

    R-base setup

--- setupForLinux.sh
+++ setupForLinux.sh
@@ -1,3 +1,4 @@
+
 sudo apt-get update
 sudo apt update
 yes Y | sudo apt install python3-pip
@@ -5,5 +6,12 @@ cd Lung_cancer
 mkdir Dataset
 cd Dataset
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1CU-eImDIGflCKbPHGdkodgtMp6RDfUjY' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1CU-eImDIGflCKbPHGdkodgtMp6RDfUjY" -O "Dataset_LUNA_16.zip"&& rm -rf /tmp/cookies.txt
+yes Y | sudo apt-get install unzip
+unzip Dataset_LUNA_16
 
+sudo apt update -qq
+sudo apt install --no-install-recommends software-properties-common dirmngr
+sudo wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+yes '\n' | sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 
+yes Y | sudo apt install --no-install-recommends r-base
